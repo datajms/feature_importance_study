376bf15b1459c2a82bb9af8ff9451e70ac1c16bb
datajms <jean.matthieu.schertzer@gmail.com>
2020-05-27 15:40:53 +0200

    first commit

---
+++ feat_imp/generate_dataset.py
@@ -0,0 +1,14 @@
+import openturns as ot
+
+
+var_conf = [
+    "day_of_week",
+    "price",
+    "ratio_shipping",
+    "nb_rating",
+    "avg_rating",
+    "nb_provider_rating",
+    "avg_provider_rating",
+    "has_multipayment",
+    "conversion_rate",
+]
